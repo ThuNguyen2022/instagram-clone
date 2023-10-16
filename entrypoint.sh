1b84b7e4fd81bc9062fd0b6eabb28a445ef60159
ThuNguyen2022 <thu.n@cs.ventures>
2023-10-16 08:54:44 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-cp /app/config/database.yml.docker /app/config/database.yml
+# cp ./config/database.yml.docker ./config/database.yml
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
